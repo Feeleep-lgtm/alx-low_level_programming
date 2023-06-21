800910221942a5da34c0494eb7020b3b7c9c1b10
Feeleep-lgtm <aliup45@gmail.com>
2023-06-20 21:23:25 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
-  *main- Entry point
-  *
-  *Return: Always 0
-  */
+ *main - Entry point
+ *
+ *Return: always 0
+ */
 int main(void)
 {
-	printf("_");
-	printf("p");
-	printf("u");
-	printf("t");
-	printf("c");
-	printf("h");
-	printf("a");
-	printf("r");
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	x = _islower('H');
+	_putchar(x + '0');
+	x = _islower('o');
+	_putchar(x + '0');
+	x = _islower(108);
+	_putchar(x + '0');
+	_putchar('\n');
+	return (0);
+}
+
+
