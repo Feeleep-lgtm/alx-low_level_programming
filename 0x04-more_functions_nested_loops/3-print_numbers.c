a54acf8f38c6b7416f25486717476312a71362cb
Feeleep-lgtm <aliup45@gmail.com>
2023-06-23 18:38:14 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers from 0-9
+ * print_numbers - print 0-9
  * return: void
  */
 
 void print_numbers(void)
 {
-	char c;
+	char x;
 
-	for (c = '0'; c <= '9'; c++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		_putchar(c);
+		_putchar(x);
 	}
 	_putchar('\n');
 }
+
 
