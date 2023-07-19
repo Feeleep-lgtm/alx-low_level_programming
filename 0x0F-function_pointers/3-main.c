0c6fc0606ef40225b87f4bb180f654bf47e9f317
Feeleep-lgtm <aliup45@gmail.com>
2023-07-19 15:32:45 -0700

    commit correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - Prints result of simple operations.
+ * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -11,8 +11,8 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int x, y;
-	char *i;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -20,24 +20,24 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	x  = atoi(argv[1]);
-	i = argv[2];
-	x = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (get_op_func(i) == NULL || i[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*i == '/' && y == 0) ||
-	    (*i == '%' && y == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(i)(x, y));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
