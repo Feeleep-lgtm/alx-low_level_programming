827e384ce46f6a9d70ca4d8f4d2d3105837ce51a
feeleep-lgtm <aliup4@gmail.com>
2023-09-05 08:07:51 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,15 +2,15 @@
 
 /**
  * create_file - Create file.
- * @filename: Pointer to the file name to create.
- * @text_content: Pointer to a string to write to the file.
+ * @filename: pointer to the name of the file to create.
+ * @text_content: pointer to a string to write to the file.
  *
  * Return: If function fails - -1.
- *         Otherwise - 1.
+ *         else - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int name, x, len = 0;
+	int fd, i, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,15 +21,14 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	name = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	x = write(fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	i = write(fd, text_content, len);
 
-	if (name == -1 || x == -1)
+	if (fd == -1 || i == -1)
 		return (-1);
 
-	close(name);
+	close(fd);
 
 	return (1);
 }
-
 
